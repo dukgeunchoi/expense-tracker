3e594cb73249e93fca25c480a4930c075afb1bf9
Dukgeun Choi <dukgeun@Dukgeuns-MacBook-Pro.local>
2025-03-31 20:50:02 +1100

    working on frontend style

---
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Sidebar.css'
+import { MdDashboard, MdAttachMoney } from "react-icons/md";
+import { FaMoneyBillTransfer } from "react-icons/fa6";
+
+
+const Sidebar = () => {
+
+  return (
+    <div className="sidebar">
+        <div className="top">
+            <span className="logo">Expense Tracker</span>
+
+            <div className="profile">
+                <img src="https://via.placeholder.com/150" alt="Profile" className="profileImg" />
+                <span className="profileName">John Doe</span>
+            </div>
+            
+            <div className="pageTitle">
+                <div className="dashboard">
+                  <MdDashboard className="icon" /> Dashboard
+                </div>
+                <div className="income">
+                  <MdAttachMoney className="icon" /> Income
+                </div>
+                <div className="expense">
+                  <FaMoneyBillTransfer className="icon" /> Expense
+                </div>
+            </div>
+        </div>
+
+        <div className="bottom">
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
