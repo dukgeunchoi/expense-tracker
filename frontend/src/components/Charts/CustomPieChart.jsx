f2891137a9673bfd40551000ff0d11bd7c0438a4
Dukgeun Choi <dukgeun@Dukgeuns-MacBook-Pro.local>
2025-04-16 19:21:09 +1000

    working on dashboard page

---
+++ frontend/src/components/Charts/CustomPieChart.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  ResponsiveContainer,
+  Legend,
+} from "recharts";
+import CustomTooltip from "./CustomTooltip";
+
+const CustomPieChart = ({
+  data,
+  label,
+  totalAmount,
+  colours,
+  showTextAnchor,
+}) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="amount"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          outerRadius={130}
+          innerRadius={100}
+        >
+          {data.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={colours[index % colours.length]}
+            />
+          ))}
+        </Pie>
+        <Tooltip content={<CustomTooltip />} />
+        <Legend />
+
+        {showTextAnchor && (
+          <>
+            <text
+              x="50%"
+              y="50%"
+              dy={-25}
+              textAnchor="middle"
+              fill="#666"
+              fontSize="14px"
+            >
+              {label}
+            </text>
+
+            <text
+              x="50%"
+              y="50%"
+              dy={8}
+              textAnchor="middle"
+              fill="#333"
+              fontSize="24px"
+              fontWeight="semi-bold"
+            >
+              {totalAmount}
+            </text>
+          </>
+        )}
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default CustomPieChart;
