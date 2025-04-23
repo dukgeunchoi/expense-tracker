c0f26d26cabd0a8cf1377bc16e5853f039ec9214
Dukgeun Choi <dukgeun@Dukgeuns-MacBook-Pro.local>
2025-04-23 12:03:47 +1000

    added backend implementatin

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
-import { register, login, getUserInfo } from "../controllers/auth.js";
+import {
+  register,
+  login,
+  getUserInfo,
+  updateUserProfile,
+  changePassword,
+} from "../controllers/auth.js";
 import { protect } from "../middleware/authMiddleware.js";
 import upload from "../middleware/uploadMiddleware.js";
 
@@ -8,7 +14,18 @@ const authRouter = Router();
 authRouter.post("/signup", register);
 authRouter.post("/login", login);
 authRouter.get("/info", protect, getUserInfo);
+authRouter.put("/update", protect, updateUserProfile);
+authRouter.put("/change-password", protect, changePassword);
 authRouter.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+  res.status(200).json({ imageUrl });
+});
+authRouter.put("/update-image", protect, upload.single("image"), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded" });
   }
